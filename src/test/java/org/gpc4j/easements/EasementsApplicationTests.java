da986a9e0dc8e8a5acfb9cec92aceb35c443c338
Lyle Harris <lyle.harris@gcpay.com>
2026-06-09 13:32:46 -0700

    Add bogus ravendb.urls for Unit Tests

--- src/test/java/org/gpc4j/easements/EasementsApplicationTests.java
+++ src/test/java/org/gpc4j/easements/EasementsApplicationTests.java
@@ -3,7 +3,7 @@ package org.gpc4j.easements;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "ravendb.urls=http://localhost:8080")
 class EasementsApplicationTests {
 
   @Test
